6c8944aa0f83d4157777039b91f0218c53918021
허세진 <heosejin222@gmail.com>
2025-05-14 21:54:52 +0900

    init

---
+++ src/main/java/USA/TEAM/PACKMON/PackmonApplication.java
@@ -0,0 +1,13 @@
+package USA.TEAM.PACKMON;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackmonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackmonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/USA/TEAM/PACKMON/PackmonApplicationTests.java
@@ -0,0 +1,13 @@
+package USA.TEAM.PACKMON;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackmonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
